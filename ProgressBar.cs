61837f7127a71ae7557766270ffe68bb28abdd6e
Erdal Ozban <eozban@hotmail.com>
2023-11-28 10:53:05 +0300

    yeni özellikler eklendi

--- ProgressBar.cs
+++ ProgressBar.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 namespace console_progress_with_task
 {
-    class ProgressBar
+        class ProgressBar
     {
         private static readonly object ConsoleWriterLock = new object();
         private int rowid = -1;
@@ -15,11 +15,12 @@ namespace console_progress_with_task
         private int titleLength;
         private int progressLength;
         private int progressMax;
+        private int progressCurrent = 0;
 
         private const string animation = @"|/-\";
         private int animationIndex = 0;
 
-        public ProgressBar(string title, int progressMax, int progressLength = 50, int titleLength = 30)
+        public ProgressBar(string title, int progressMax, int progressLength = 50, int titleLength = 35)
         {
             lock (ConsoleWriterLock)
             {
@@ -31,6 +32,24 @@ namespace console_progress_with_task
             this.progressLength = progressLength;
             this.titleLength = titleLength;
             this.progressMax = progressMax;
+        }
+
+        public void step()
+        {
+            this.setProgress(++this.progressCurrent);
+        }
+
+        public void setTitle(string title)
+        {
+            lock (ConsoleWriterLock)
+            {
+                int left = Console.CursorLeft;
+                int top = Console.CursorTop;
+                this.title = title;
+                Console.SetCursorPosition(0, this.rowid);
+                Console.Write(title.Length >= titleLength ? title.Substring(0, titleLength) : title + new string(' ', titleLength - title.Length));
+                Console.SetCursorPosition(left, top);
+            }
         }
 
         public void setProgress(int progress)
@@ -47,11 +66,12 @@ namespace console_progress_with_task
                         int charCount = (int)progressLength * percent / 100;
 
                         Console.SetCursorPosition(0, this.rowid);
-                        Console.Write("{0} [{1,3}%] [{2}] [{3}] Max:{4}",
-                            title + new string(' ', titleLength - title.Length),
+                        Console.Write("{0} [{1,3}%] [{2}] [{3}] {4}/{5}",
+                            title.Length >= titleLength ? title.Substring(0, titleLength) : title + new string(' ', titleLength - title.Length),
                             percent,
                             new string('#', charCount) + new string('-', progressLength - charCount),
                             animation[animationIndex++ % animation.Length],
+                            progress,
                             progressMax);
                         Console.SetCursorPosition(left, top);
                     }
